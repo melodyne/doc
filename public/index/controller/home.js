7434ca284f552f6d60bac1d7d1439591a47b0166
chen <29512450@qq.com>
2017-09-13 17:31:29 +0800

    首页边框

--- public/index/controller/home.js
+++ public/index/controller/home.js
@@ -12,5 +12,6 @@ angular.module('myApp.home', ['ngRoute'])
 .controller('HomeCtrl', function($scope,$http) {
   $http.get("/api/project/index").then(function (response) {
     $scope.datas = response['data'];
+    $('.pro_menu').css('border', '1px solid #c9c9c9');// 给导航加边框
   });
 });
