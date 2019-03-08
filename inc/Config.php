2ed63d56198433bf90c7323b9a8264b2ff03b062
ullone <ullone@163.com>
2019-03-08 15:46:51 +0800

    更新了扩展

--- inc/Config.php
+++ inc/Config.php
@@ -1,13 +1,13 @@
 <?php 
 class Config{
-    public static $dbHost="localhost";
+    public static $dbHost="127.0.0.1";
 	
     //public static $dbUser="wzswanji_chen";
     //public static $dbPwd="chen100123";
     //public static $dbName="wzswanji_chen";
 	
 	public static $dbUser="root";
-    public static $dbPwd="123456";
+    public static $dbPwd="root";
     public static $dbName="doc";
 }
 ?>
--- thinkphp/library/think/route/dispatch/Controller.php
+++ thinkphp/library/think/route/dispatch/Controller.php
@@ -2,17 +2,29 @@
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006~2017 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-namespace think;
+namespace think\route\dispatch;
 
-// ThinkPHP 引导文件
-// 加载基础文件
-require __DIR__ . '/base.php';
-// 执行应用
-App::run()->send();
+use think\route\Dispatch;
+
+class Controller extends Dispatch
+{
+    public function exec()
+    {
+        // 执行控制器的操作方法
+        $vars = array_merge($this->request->param(), $this->param);
+
+        return $this->app->action(
+            $this->dispatch, $vars,
+            $this->rule->getConfig('url_controller_layer'),
+            $this->rule->getConfig('controller_suffix')
+        );
+    }
+
+}
